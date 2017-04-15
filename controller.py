4ec710ca3e7d1da1d40f270f807ef184e90af335
FLANNERY-LAPTOP\Flannery <flancor@swbell.net>
2017-04-15 11:44:03 -0400

    Recommended artists returned as a dictionary -- can display artist names

--- controller.py
+++ controller.py
@@ -4,13 +4,10 @@ import spotipy
 import spotipy.util as util
 
 
-SPOTIPY_CLIENT_ID = 'id'
-SPOTIPY_CLIENT_SECRET = 'secret'
-SPOTIPY_REDIRECT_URI = 'http://localhost:8080'
-SCOPE = 'playlist-read-private playlist-modify-private'
-CACHE = '.spotipyoauthcache'
-
-debug = False
+SPOTIPY_CLIENT_ID = '9da0c8416b0e429a858497713be2b92a'
+SPOTIPY_CLIENT_SECRET = 'a73170daa78142a1a22371cbe7c9f8f2'
+SPOTIPY_REDIRECT_URI = 'http://localhost:8080/callback/'
+CACHE = '.spotipyauthcache'
 
 def show_tracks(tracks):
 	for i, item in enumerate(tracks['items']):
@@ -18,10 +15,12 @@ def show_tracks(tracks):
 		print ("   %d %32.32s %s" % (i, track['artists'][0]['name'],
 			track['name']))
 			
-def authenticate_user(token):
+def authenticate_user(username):
 	scope = "playlist-read-private playlist-modify-private"
+	token = util.prompt_for_user_token(username, scope, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI)
 	sp = spotipy.Spotify(auth=token)
 	return sp
+
 	
 def display_playlists(sp):
 	playlists = sp.current_user_playlists()
@@ -40,47 +39,47 @@ def create_playlist(sp, name, age, genres, artists=None, tracks=None):
 	    artists = artists_from_year_range_and_genres(sp, year_range, genres)
 	
 	recommended_tracks = []
-	"""
-    j = 0
+	'''
+	j = 0
 	if len(artists) > 0:
-	    while(j + 5 < len(artists)):
-		recommended_tracks += sp.recommendations(seed_artists=artists[j:j+5], limit=10)["tracks"]
-		j += 5
-		recommended_tracks += sp.recommendations(seed_artists=artists[j:], limit=10)["tracks"]
-	"""
-	#recommended_tracks = sp.recommendations(seed_genres = genres, seed_tracks = tracks, limit=90)["tracks"]
-	recommended_tracks = sp.recommendations(seed_genres = genres, limit=90)["tracks"]
+		while(j + 5 < len(artists)):
+			recommended_tracks += sp.recommendations(seed_artists=artists[j:j+5], limit=10)["tracks"]
+			j += 5
+		if j < len(artists):
+			recommended_tracks += sp.recommendations(seed_artists=artists[j:], limit=10)["tracks"]
+	
+	recommended_tracks += sp.recommendations(seed_genres = genres, limit=(90 - len(recommended_tracks)))["tracks"]
 
-        nameOne = name
-        nameTwo = name
-        nameThree = name
+	nameOne = name
+	nameTwo = name
+	nameThree = name
         
 	track_uris = []    
-	for track in recommended_tracks[:29]:
-	    track_uris.append(track["uri"])
+	for track in recommended_tracks[:30]:
+		track_uris.append(track["uri"])
 	nameOne += "1"
 	playlistOne = sp.user_playlist_create(user_id, nameOne, public=False)
 	playlist_id_one = playlistOne["id"]
-        sp.user_playlist_add_tracks(user_id, playlist_id_one, track_uris)
+	sp.user_playlist_add_tracks(user_id, playlist_id_one, track_uris)
 
-        track_uris = []
-        for track in recommended_tracks[30:59]:
-            track_uris.append(track["uri"])
+	track_uris = []
+	for track in recommended_tracks[30:60]:
+		track_uris.append(track["uri"])
 	nameTwo += "2"
-        playlist = sp.user_playlist_create(user_id, nameTwo, public=False)
+	playlist = sp.user_playlist_create(user_id, nameTwo, public=False)
 	playlist_id_two = playlist["id"]
-        sp.user_playlist_add_tracks(user_id, playlist_id_two, track_uris)
+	sp.user_playlist_add_tracks(user_id, playlist_id_two, track_uris)
 
-        track_uris = []
-        for track in recommended_tracks[60:]:
-            track_uris = []
-            track_uris.append(track["uri"])
+	track_uris = []
+	for track in recommended_tracks[60:]:
+		track_uris.append(track["uri"])
 	nameThree += "3"
 	playlist = sp.user_playlist_create(user_id, name, public=False)
 	playlist_id_three = playlist["id"]
-        sp.user_playlist_add_tracks(user_id, playlist_id_three, track_uris)
+	sp.user_playlist_add_tracks(user_id, playlist_id_three, track_uris)
                 
 	return (playlist_id_one, playlist_id_two, playlist_id_three) 
+	'''
 	
 def display_playlist_tracks(sp, playlist_id):
 	userID = sp.current_user()["id"]
@@ -112,36 +111,28 @@ def albums_from_year_range(sp, range):
 	
 def artists_from_year_range_and_genres(sp, range, genres):
 	results = sp.search(q='year:' + str(range[0]) + '-' + str(range[1]), type='album',limit=50)
-	#print(results)
 
-	if debug:
-		print(results)
 	albums = results['albums']['items']
 
-	artists = []
+	artists = {}
 	for album in albums:
-		if debug:
-			print(album['name'])
 		for artist in album['artists']:
-			if artist['id'] not in artists:
-				if debug:
-					print(artist['name'])
-				artists.append(artist['id'])
-	if debug:
-		print(artists)
+			if artist['id'] not in artists.keys():
+				artists[artist['id']] = artist['name']
 	
 	bad_artists = []
 			
-	for id in artists:
+	for id in artists.keys():
 		current_artist = sp.artist(id)
-		if debug:
-			print(current_artist['genres'])
 		if set(genres).isdisjoint(set(current_artist['genres'])):
 			bad_artists.append(current_artist['id'])
 	
 	for artist in bad_artists:		
-		artists.remove(artist)
-			
+		del artists[artist]
+	
+	for name in artists.values():
+		print(name)
+		
 	return artists
 
 			
@@ -166,17 +157,8 @@ def upvote(sp, playlist_id, track):
 if __name__ == '__main__':
 	sp = authenticate_user("fcurrin")
 	user_id = sp.current_user()["id"]
-	#debug = True
-	#display_playlists(sp)
 
-	file = open("genres", "w")
-	file.write(str(sp.recommendation_genre_seeds()))
-	file.close()
-	
-	#new_playlist = create_playlist(sp, "test_artists_90", 90, ["jazz", "big band", "classical"])
-	#display_playlist_tracks(sp, new_playlist)
-	#print(sp.recommendation_genre_seeds())
-
+	'''
 	new_playlist_one = create_playlist(sp, "test_artists_90", 90, ["jazz", "big band", "classical"])[0]
 	new_playlist_two = create_playlist(sp, "test_artists_90", 90, ["jazz", "big band", "classical"])[1]
 	new_playlist_three = create_playlist(sp, "test_artists_90", 90, ["jazz", "big band", "classical"])[2]
@@ -184,6 +166,6 @@ if __name__ == '__main__':
 	display_playlist_tracks(sp, new_playlist_one)
 	display_playlist_tracks(sp, new_playlist_two)
 	display_playlist_tracks(sp, new_playlist_three)
+	'''
 	
-	#new_playlist = create_playlist(sp, "test_artists_75", 75, ["jazz", "big band", "classical"])
-	#display_playlist_tracks(sp, new_playlist)
+	artists_from_year_range_and_genres(sp, get_year_range(90), ["jazz", "big band", "classical"])
