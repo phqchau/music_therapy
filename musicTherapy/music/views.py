4e32f311d9ee4a0a54e5ed1d5da73fadc15cf39c
Chau P <cqpham15@earlham.edu>
2017-04-16 23:59:10 -0400

    fixed redirect page from processPlaylist to chooseArtists

--- musicTherapy/music/views.py
+++ musicTherapy/music/views.py
@@ -13,7 +13,7 @@ import ast
 # Create your views here.
 SPOTIPY_CLIENT_ID = secrets.SPOTIPY_CLIENT_ID
 SPOTIPY_CLIENT_SECRET = secrets.SPOTIPY_CLIENT_SECRET
-genres = ['afrobeat', 'alt-rock', 'alternative', 'black-metal', 'bluegrass', 'blues', 'bossanova', 'brazil', 'breakbeat', 'british', 'cantopop', 'chicago-house', 'children', 'chill', 'classical', 'club', 'comedy', 'country', 'dance', 'dancehall', 'death-metal', 'deep-house', 'disco', 'disney', 'drum-and-bass', 'dub', 'dubstep', 'edm', 'electro', 'electronic', 'emo', 'folk', 'forro', 'french', 'funk', 'garage', 'german', 'gospel', 'goth', 'groove', 'guitar', 'happy', 'hard-rock', 'hardcore', 'hardstyle', 'heavy-metal', 'hip-hop', 'holidays', 'honky-tonk', 'house', 'idm', 'indian', 'indie', 'indie-pop', 'industrial', 'iranian', 'j-pop', 'j-rock', 'jazz', 'k-pop', 'kids', 'latin', 'latino', 'malay', 'mandopop', 'metal', 'metalcore', 'minimal-techno', 'movies', 'mpb', 'new-age', 'new-release', 'opera', 'pagode', 'party', 'philippines-opm', 'piano', 'pop', 'pop-film', 'post-dubstep', 'power-pop', 'progressive-house', 'psych-rock', 'punk', 'punk-rock', 'r-n-b', 'rainy-day', 'reggae', 'reggaeton', 'road-trip', 'rock', 'rock-n-roll', 'rockabilly', 'romance', 'sad', 'salsa', 'samba', 'sertanejo', 'show-tunes', 'ska', 'sleep', 'soul', 'soundtracks', 'spanish', 'study', 'summer', 'swedish', 'synth-pop', 'tango', 'techno', 'trance', 'trip-hop', 'turkish', 'work-out', 'world-music']
+genres = ['afrobeat', 'alt-rock', 'alternative', 'black-metal', 'bluegrass', 'blues', 'bossanova', 'brazil', 'breakbeat', 'british', 'cantopop', 'chicago-house', 'children', 'chill', 'classical', 'club', 'comedy', 'country', 'dance', 'dancehall', 'death-metal', 'deep-house', 'disco', 'disney', 'drum-and-bass', 'dub', 'dubstep', 'edm', 'electro', 'electronic', 'emo', 'folk', 'forro', 'french', 'funk', 'garage', 'german', 'gospel', 'goth', 'groove', 'guitar', 'happy', 'hard-rock', 'hardcore', 'hardstyle', 'heavy-metal', 'hip-hop', 'holidays', 'honky-tonk', 'house', 'idm', 'indian', 'indie', 'indie-pop', 'industrial', 'iranian', 'j-pop', 'j-rock', 'jazz', 'k-pop', 'kids', 'latin', 'latino', 'malay', 'mandopop', 'metal', 'metalcore', 'minimal-techno', 'movies', 'mpb', 'new-age', 'opera', 'pagode', 'party', 'philippines-opm', 'piano', 'pop', 'pop-film', 'post-dubstep', 'power-pop', 'progressive-house', 'psych-rock', 'punk', 'punk-rock', 'r-n-b', 'rainy-day', 'reggae', 'reggaeton', 'road-trip', 'rock', 'rock-n-roll', 'rockabilly', 'romance', 'sad', 'salsa', 'samba', 'sertanejo', 'show-tunes', 'ska', 'sleep', 'soul', 'soundtracks', 'spanish', 'study', 'summer', 'swedish', 'synth-pop', 'tango', 'techno', 'trance', 'trip-hop', 'turkish', 'work-out', 'world-music']
 artists = []
 tracks = []
 
@@ -21,14 +21,9 @@ def callback(request):
     return render(request, 'music/spotifyLoginFinish.html',{})
 
 def index(request):
-	#latest_question_list = Question.objects.order_by('-pub_date')[:5]
 	redirect_uri = 'http://musictherapy.pythonanywhere.com/music/authUser/'
 	context = {'redirect_uri': redirect_uri,}
 	return render(request, 'music/index.html', context)
-
-'''def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'music/detail.html', {'question':question})'''
 
 def play(request, playlist_id):
 	if request.session.has_key('user_id'):
@@ -104,10 +99,9 @@ def chooseArtists(request):
 	request.session['age'] = age
 	artists = controller.artists_from_year_range_and_genres(sp, genres, age)
 	artist_list = []
-	#ArtistSeeds.objects.all().delete()
+
 	for artist_id, artist_name in artists.items():
 		request.session['artist'] = (artist_id, artist_name)
-		#ArtistSeeds.objects.create(artist_name=artist_name, artist_id=artist_id)
 		artist_list.append(artist_name)
 	return render(request, 'music/chooseArtists.html', {'artist_list':artist_list})
 
@@ -121,8 +115,11 @@ def processPlaylist(request):
 				artistsFetched.append(artist)
 
 	except:
-		# Redisplay the question voting form.
-		return render(request, 'music/createPlaylist.html', {'genres': genres, 'artists':artists, 'tracks':tracks,'error_message': "You didn't select a choice."})		
+		artist_list = []
+		if request.session.has_key('artist'):
+			artist_id, artist_name = request.session['artist']
+			artist_list.append(artist_name)
+		return render(request, 'music/chooseArtists.html', {'artist_list':artist_list,'error_message': "You didn't provide a playlist name."})		
 	else:
 		if request.session.has_key('user_id'):
 			user_id = request.session['user_id']
@@ -135,11 +132,9 @@ def processPlaylist(request):
 		if request.session.has_key('age'):
 			age = request.session['age']
 
-		#user_id = "1262880145"
 		artist_ids = []
 		for artist in artistsFetched:
 			if request.session.has_key('artist'):
-			#artist_id = ArtistSeeds.objects.filter(artist_name=artist).values()[0]['artist_id']
 				artist_id = request.session['artist'][0]
 				artist_ids.append(artist_id)
 
@@ -158,10 +153,6 @@ def processPlaylist(request):
 		if request.session.has_key('age'): del request.session['age']
 		if request.session.has_key('artist'): del request.session['artist']
 		return render(request, 'music/play.html', {'playlist_uri':playlist_uri})
-		#return HttpResponse("Playlist has been created!\nYou're looking at {0}".format(new_playlist))
-		#return render(request, 'polls/play.html', {'playlist':new_playlist,})
-#		return HttpResponseRedirect('polls/play.html?playlist_id={0}'.format(new_playlist))
-
 
 def viewPlaylist(request):
 	if request.session.has_key('user_id'):
@@ -174,5 +165,4 @@ def viewPlaylist(request):
 		playlist_name = Seeds.playlist_name
 		list_of_playlist_tuples.append((playlist_name,playlist_id))
 
-	#return HttpResponse(list_of_playlist_tuples)
 	return render(request, 'music/viewPlaylist.html', {'list_of_playlist_tuples':list_of_playlist_tuples})		
