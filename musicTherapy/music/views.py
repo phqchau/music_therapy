a1f66de3f46310a08cd6498d9ceb1d1c228d7f0a
Chau P <cqpham15@earlham.edu>
2017-04-17 01:16:59 -0400

    some html formatting

--- musicTherapy/music/views.py
+++ musicTherapy/music/views.py
@@ -10,7 +10,6 @@ import requests
 import json
 import ast
 
-# Create your views here.
 SPOTIPY_CLIENT_ID = secrets.SPOTIPY_CLIENT_ID
 SPOTIPY_CLIENT_SECRET = secrets.SPOTIPY_CLIENT_SECRET
 genres = ['afrobeat', 'alt-rock', 'alternative', 'black-metal', 'bluegrass', 'blues', 'bossanova', 'brazil', 'breakbeat', 'british', 'cantopop', 'chicago-house', 'children', 'chill', 'classical', 'club', 'comedy', 'country', 'dance', 'dancehall', 'death-metal', 'deep-house', 'disco', 'disney', 'drum-and-bass', 'dub', 'dubstep', 'edm', 'electro', 'electronic', 'emo', 'folk', 'forro', 'french', 'funk', 'garage', 'german', 'gospel', 'goth', 'groove', 'guitar', 'happy', 'hard-rock', 'hardcore', 'hardstyle', 'heavy-metal', 'hip-hop', 'holidays', 'honky-tonk', 'house', 'idm', 'indian', 'indie', 'indie-pop', 'industrial', 'iranian', 'j-pop', 'j-rock', 'jazz', 'k-pop', 'kids', 'latin', 'latino', 'malay', 'mandopop', 'metal', 'metalcore', 'minimal-techno', 'movies', 'mpb', 'new-age', 'opera', 'pagode', 'party', 'philippines-opm', 'piano', 'pop', 'pop-film', 'post-dubstep', 'power-pop', 'progressive-house', 'psych-rock', 'punk', 'punk-rock', 'r-n-b', 'rainy-day', 'reggae', 'reggaeton', 'road-trip', 'rock', 'rock-n-roll', 'rockabilly', 'romance', 'sad', 'salsa', 'samba', 'sertanejo', 'show-tunes', 'ska', 'sleep', 'soul', 'soundtracks', 'spanish', 'study', 'summer', 'swedish', 'synth-pop', 'tango', 'techno', 'trance', 'trip-hop', 'turkish', 'work-out', 'world-music']
@@ -21,6 +20,9 @@ def index(request):
 	redirect_uri = 'http://musictherapy.pythonanywhere.com/music/authUser/'
 	context = {'redirect_uri': redirect_uri,}
 	return render(request, 'music/index.html', context)
+
+def about(request):
+	return render(request, 'music/about.html', {})
 
 def play(request, playlist_id):
 	if request.session.has_key('user_id'):
